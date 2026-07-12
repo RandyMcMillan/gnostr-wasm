5763b2f9c24e3eeae73173f70120b0c62a79521c
randymcmillan <randymcmillan@protonmail.com>
2026-07-12 11:29:49 -0400

    src/wry_app.rs:ratzilla

--- src/wry_app.rs
+++ src/wry_app.rs
@@ -1,39 +1,85 @@
-use std::{error::Error, io, path::PathBuf};
+use std::{
+    error::Error,
+    io,
+    net::{SocketAddr, TcpStream},
+    process::{Child, Command, Stdio},
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 
 use tao::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-use url::Url;
 use wry::WebViewBuilder;
 
+const HOST: &str = "127.0.0.1";
+const PORT: u16 = 8080;
+const URL: &str = "http://127.0.0.1:8080";
+
 pub fn run() -> Result<(), Box<dyn Error>> {
+    let server = Arc::new(Mutex::new(Some(spawn_trunk_serve()?)));
+    wait_for_server()?;
+
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("gnostr-wasm")
         .build(&event_loop)?;
 
-    let index_path = std::env::current_dir()?.join(PathBuf::from("dist/index.html"));
-    let index_url = Url::from_file_path(&index_path).map_err(|_| {
-        io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("invalid file path: {}", index_path.display()),
-        )
-    })?;
+    let _webview = WebViewBuilder::new().with_url(URL).build(&window)?;
 
-    let _webview = WebViewBuilder::new()
-        .with_url(index_url.as_str())
-        .build(&window)?;
-
+    let server = Arc::clone(&server);
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
+
         if let Event::WindowEvent {
             event: WindowEvent::CloseRequested,
             ..
         } = event
         {
+            if let Some(mut child) = server.lock().ok().and_then(|mut slot| slot.take()) {
+                let _ = child.kill();
+                let _ = child.wait();
+            }
             *control_flow = ControlFlow::Exit;
         }
     });
+}
+
+fn spawn_trunk_serve() -> Result<Child, io::Error> {
+    Command::new("trunk")
+        .args([
+            "serve",
+            "--address",
+            HOST,
+            "--port",
+            "8080",
+            "--no-autoreload",
+            "true",
+            "--open",
+            "false",
+        ])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+}
+
+fn wait_for_server() -> Result<(), io::Error> {
+    let addr: SocketAddr = format!("{HOST}:{PORT}").parse().map_err(|err| {
+        io::Error::new(io::ErrorKind::InvalidInput, format!("invalid address: {err}"))
+    })?;
+
+    for _ in 0..120 {
+        if TcpStream::connect_timeout(&addr, Duration::from_millis(250)).is_ok() {
+            return Ok(());
+        }
+        thread::sleep(Duration::from_millis(500));
+    }
+
+    Err(io::Error::new(
+        io::ErrorKind::TimedOut,
+        "trunk serve did not start on 127.0.0.1:8080",
+    ))
 }
