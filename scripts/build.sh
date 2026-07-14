d59d773ecced92a3079d4f217ba88f4d0751673d
randymcmillan <randymcmillan@protonmail.com>
2026-07-14 09:12:05 -0400

    scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,9 +11,9 @@ ensure_wasm_target
 ensure_cargo_bundle
 ensure_trunk
 
+trunk build --release --dist dist
 cargo bundle --bin gnostr-wasm --release --features wry-app
 cargo bundle --bin gnostr-cloud --release --features wry-app
-trunk build --release --dist dist
 
 printf 'Built %s\n' "target/release/bundle/"
 printf 'Built %s\n' "dist/"
